00dbecd500605755e14ea5df49b82ac0662f56fc
Toma Dovydene <toma.dovydene@gmail.com>
2023-06-12 10:14:27 +0300

    2023-06-12 10:10 workouts paginator

--- mysite/journal/views.py
+++ mysite/journal/views.py
@@ -89,8 +89,13 @@ def workouts(request):
     elif sort_by == 'date':
         workouts = workouts.order_by('date')
 
+    paginator = Paginator(workouts, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'workouts': workouts,
+        'workouts': page_obj,
         'query': query if query else '',
     }
     return render(request, 'workouts.html', context=context)
